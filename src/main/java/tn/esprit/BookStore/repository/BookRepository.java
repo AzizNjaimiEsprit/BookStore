59a340f2d91bbea23e0b00f5877ebbe704e547f7
Samar NEJI <samar.neji@esprit.tn>
2021-01-06 21:22:27 +0100

    models added

---
+++ src/main/java/tn/esprit/BookStore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.BookStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository {
+
+}
