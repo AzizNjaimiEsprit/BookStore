82c3f98ae4e4c58d25650bdbb302f964d693a3d1
Med Aziz Njaimi <mohamedaziz.njaimi@esprit.tn>
2021-01-23 09:04:46 +0100

    Rectifications added

--- src/main/java/tn/esprit/BookStore/repository/SMS_Service.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.BookStore.service;
-
-import tn.esprit.BookStore.model.Coupon;
-import tn.esprit.BookStore.model.User;
-
-public interface SMS_Service {
-    public boolean SendDeclineSMS(User user);
-    public boolean sendCouponSMS(User user, Coupon coupon);
-}
